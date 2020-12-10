add0c8779ce5b5714ceb76cb5ec2e0ebbcd16d14
bakeworks <mail@bake.works>
2020-12-10 16:53:01 +1100

    work in progress

---
+++ src/util/array.js
@@ -0,0 +1,47 @@
+function sum (values) {
+  values.reduce((accum, val) => accum + val, 0.0)
+}
+
+// Returns array of elements in array 'a' which ARE NOT present in array 'b'
+function difference (a, b, cmpfn) {
+  return a.filter(p => b.findIndex(q => cmpfn(p, q)) === -1)
+}
+
+// Returns elements in array 'a' which ARE present in array 'b'
+function intersection (a, b, cmpfn) {
+  return a.filter(p => b.findIndex(q => cmpfn(p, q)) !== -1)
+}
+
+// Returns union of elements in arrays 'a' and 'b'
+function union (a, b, cmpfn) {
+  const both = [a, b]
+  const result = []
+  both.forEach(c => {
+    c.forEach(p => {
+      if (result.findIndex(q => cmpfn(p, q) === -1)) {
+        result.push(p)
+      }
+    })
+  })
+  return result
+}
+
+// cmpfn should return true if match, false if not
+// given two objects to compare
+function unique (array, cmpfn) {
+  const result = []
+  array.forEach(a => {
+    if (result.findIndex(b => cmpfn(a, b)) === -1) {
+      result.push(a)
+    }
+  })
+  return result
+}
+
+module.exports = {
+  sum,
+  difference,
+  intersection,
+  union,
+  unique
+}
