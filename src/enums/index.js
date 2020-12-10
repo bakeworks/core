baf9646cd252862b48d6f215818fab2396f1ddf8
bakeworks <mail@bake.works>
2020-12-11 10:07:41 +1100

    work in progress

--- src/enums/index.js
+++ src/enums/index.js
@@ -1,11 +1,6 @@
-const product = require('./product')
-const recipe = require('./recipe')
-const user = require('./user')
-const bool = require('./bool')
-
 module.exports = {
-  product,
-  recipe,
-  user,
-  bool
+  product: require('./product'),
+  recipe: require('./recipe'),
+  user: require('./user'),
+  bool: require('./bool')
 }
