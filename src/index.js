fd4d86c28de6f1ad52b036383028862c90cd5590
bakeworks <mail@bake.works>
2020-12-11 10:00:33 +1100

    work in progress

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-// const enums = require('./enums')
+const enums = require('./enums')
 const util = require('./util')
 
-function dummy () {
-  return 'DUMMY FN'
-}
-
 module.exports = {
-  // enums,
-  util,
-  dummy
+  enums,
+  util
 }
