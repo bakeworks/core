f62ec1cfaff0b0c75ddfbb64471e14df5c531b1d
bakeworks <mail@bake.works>
2022-04-15 12:34:19 +1000

    work in progress

--- src/util/db.js
+++ src/util/db.js
@@ -2,12 +2,12 @@
  * database utility functions
  */
 
-// toObjs maybe array or Map.
+// toObjs maybe array or object.
 // If map it should be keyed by expected values of fromObj[fromProp] and toProp will be ignore
 function resolveJoin(fromName, fromObj, fromProp, toName, toObjs, toProp = '_id') {
   const fromVal = fromObj[fromProp]
-  const result = toObjs instanceof Map
-    ? toObjs.get(fromVal)
+  const result = typeof toObjs === 'object'
+    ? toObjs[fromVal]
     : toObjs.find(e => e[toProp] === fromVal)
   if (result === undefined) {
     throw new Error(`resolveJoin failed: ${fromName}.${fromProp}=${fromVal} not found in ${toObjs.length} ${toName}.${toProp}`)
