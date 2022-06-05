269bae07f9cf682ff74e8a1aae45c87462bba8ee
bakeworks <mail@bake.works>
2022-06-05 10:35:18 +1000

    work in progress

--- src/util/debug.js
+++ src/util/debug.js
@@ -1,7 +1,11 @@
 const fs = require('fs')
 
 function dump(path, content) {
-  fs.writeFile(path, content)
+  fs.writeFile(path, content, err => {
+    if (err) {
+      console.log(`Error writing file "${path}" : ${err}`)
+    }
+  })
 }
 
 module.exports = {
