5716b621ed6b173d1723a901dc261209ef829ef5
bakeworks <mail@bake.works>
2022-06-28 10:24:36 +1000

    work in progress

--- src/util/response.js
+++ src/util/response.js
@@ -34,7 +34,7 @@ See "403 substatus error codes for IIS" for possible reasons of why the webserve
   is refusing to fulfill the request.
 */
 
-const TAG = {
+const ERROR_CODES = {
   INVALID_EMAIL: 'INVALID_EMAIL',
   ACCOUNT_NOT_ACTIVE: 'ACCOUNT_NOT_ACTIVE',
   INVALID_PASSWORD: 'INVALID_PASSWORD',
@@ -50,42 +50,37 @@ const TAG = {
   USER_NOT_AUTHORISED: 'USER_NOT_AUTHORISED'
 }
 
-const ERROR = {
+const ERROR_LABELS = {
+  [ERROR_CODES.INVALID_EMAIL]: 'Invalid email.',
+  [ERROR_CODES.ACCOUNT_NOT_ACTIVE]: 'Account is not active.',
+  [ERROR_CODES.INVALID_PASSWORD]: 'Invalid password.',
+  [ERROR_CODES.USER_WITH_EMAIL_EXISTS]: 'An account with that email already exists.',
+  [ERROR_CODES.INVALID_TOKEN]: 'Invalid authentication token.',
+  [ERROR_CODES.EXPIRED_TOKEN]: 'Authentication token has expired.',
+  [ERROR_CODES.MISSING_TOKEN]: 'Authentication token is missing.',
+  [ERROR_CODES.INVALID_SCOPE]: 'Invalid scope in authentication token.',
+  [ERROR_CODES.EXTERNAL_API_ERROR]: 'External API error.',
+  [ERROR_CODES.DB_ERROR]: 'Database error.',
+  [ERROR_CODES.MAILER_ERROR]: 'Mailer error.',
+  [ERROR_CODES.SERVER_ERROR]: 'Internal server error.',
+  [ERROR_CODES.USER_NOT_AUTHORISED]: 'User not authorised.',
+}
 
-  ...TAG,
+const HTTP_CODES = {
+  [ERROR_CODES.INVALID_EMAIL]: 401,
+  [ERROR_CODES.ACCOUNT_NOT_ACTIVE]: 401,
+  [ERROR_CODES.INVALID_PASSWORD]: 401,
+  [ERROR_CODES.USER_WITH_EMAIL_EXISTS]: 401,
+  [ERROR_CODES.INVALID_TOKEN]: 401,
+  [ERROR_CODES.EXPIRED_TOKEN]: 401,
+  [ERROR_CODES.MISSING_TOKEN]: 401,
+  [ERROR_CODES.INVALID_SCOPE]: 401,
 
-  LABEL: {
-    [TAG.INVALID_EMAIL]: 'Invalid email.',
-    [TAG.ACCOUNT_NOT_ACTIVE]: 'Account is not active.',
-    [TAG.INVALID_PASSWORD]: 'Invalid password.',
-    [TAG.USER_WITH_EMAIL_EXISTS]: 'An account with that email already exists.',
-    [TAG.INVALID_TOKEN]: 'Invalid authentication token.',
-    [TAG.EXPIRED_TOKEN]: 'Authentication token has expired.',
-    [TAG.MISSING_TOKEN]: 'Authentication token is missing.',
-    [TAG.INVALID_SCOPE]: 'Invalid scope in authentication token.',
-    [TAG.EXTERNAL_API_ERROR]: 'External API error.',
-    [TAG.DB_ERROR]: 'Database error.',
-    [TAG.MAILER_ERROR]: 'Mailer error.',
-    [TAG.SERVER_ERROR]: 'Internal server error.',
-    [TAG.USER_NOT_AUTHORISED]: 'User not authorised.',
-  },
+  [ERROR_CODES.USER_NOT_AUTHORISED]: 403, // forbidden
 
-  HTTP_CODE: {
-    [TAG.INVALID_EMAIL]: 401,
-    [TAG.ACCOUNT_NOT_ACTIVE]: 401,
-    [TAG.INVALID_PASSWORD]: 401,
-    [TAG.USER_WITH_EMAIL_EXISTS]: 401,
-    [TAG.INVALID_TOKEN]: 401,
-    [TAG.EXPIRED_TOKEN]: 401,
-    [TAG.MISSING_TOKEN]: 401,
-    [TAG.INVALID_SCOPE]: 401,
-
-    [TAG.USER_NOT_AUTHORISED]: 403, // forbidden
-
-    [TAG.EXTERNAL_API_ERROR]: 500,
-    [TAG.DB_ERROR]: 500,
-    [TAG.SERVER_ERROR]: 500,
-  }
+  [ERROR_CODES.EXTERNAL_API_ERROR]: 500,
+  [ERROR_CODES.DB_ERROR]: 500,
+  [ERROR_CODES.SERVER_ERROR]: 500,
 }
 
 function success(req, payload) {
@@ -96,12 +91,12 @@ function success(req, payload) {
   }
 }
 
-function error(req, tag, details) {
+function error(req, code, details) {
   const res = {
     success: false,
     req,
     error: {
-      tag,
+      code,
       details
     }
   }
@@ -115,7 +110,7 @@ function stackSuccess(req, prior) {
 
 function stackError(req, prior) {
   console.debug(`core/qr/stackError(req: ${req}, prior: ${JSON.stringify(prior)})`)
-  return error(req, prior.error.tag, prior.error.details)
+  return error(req, prior.error.code, prior.error.details)
 }
 
 function stack(req, prior) {
@@ -127,15 +122,15 @@ function stack(req, prior) {
 function stringify(res) {
   return res.success
     ? `${res.req}: SUCCESS`
-    : `${res.error.req}: ERROR : ${res.error ? res.error.tag : 'NULL_ERROR'}`
+    : `${res.error.req}: ERROR : ${res.error ? res.error.code : 'NULL_ERROR'}`
 }
 
 function httpCode(res) {
-  const code = ERROR.HTTP_CODE[res.error.tag]
+  const code = HTTP_CODE[res.error.code]
   if (!code) {
-    console.log('==================================================')
-    console.log(`core/qr/httpCode(res.error: ${JSON.stringify(res.error)})`)
-    console.log('==================================================')
+    console.debug('==============================================================================')
+    console.debug(`bakeworks-core/util/response/httpCode(res.error: ${JSON.stringify(res.error)})`)
+    console.debug('==============================================================================')
   }
   return code
 }
@@ -148,7 +143,9 @@ function toRouter(routerResponse, requestResult) {
 }
 
 export default {
-  ERROR,
+  ...ERROR_CODES, // splat to make usage concise
+  ERROR_LABELS,
+  HTTP_CODES,
   success,
   error,
   httpCode,
