0ffc4396e4e499d49bf75b7b3fdb0dd0f729e6b9
bakeworks <mail@bake.works>
2022-07-05 15:20:19 +1000

    work in progress

--- src/db/target.js
+++ src/db/target.js
@@ -1,4 +1,4 @@
-import schemas from './schemas/index.js'
+import { v2 as schemaV2, v3 as schemaV3 } from './schemas/index.js'
 
 // args: { schemaName, dbName, collName }
 // if schemaName is undefined set to V3 schema name
@@ -20,10 +20,10 @@ function resolve(args) {
 */
 const bakeworks = {}
 // console.debug(`target.js: v3=${v3}  v3=${v3}  v3.dbs=${v3.dbs}  v3.dbs.bakeworks=${v3.dbs.bakeworks}  v3.dbs.bakeworks.collections=${v3.dbs.bakeworks.collections}`)
-for (const [key, coll] of Object.entries(schemas.v3.dbs.bakeworks.collections)) {
+for (const [key, coll] of Object.entries(schemaV3.dbs.bakeworks.collections)) {
   bakeworks[key] = {
-    schemaName: schemas.v3.name,
-    dbName: schemas.v3.dbs.bakeworks.name,
+    schemaName: schemaV3.name,
+    dbName: schemaV3.dbs.bakeworks.name,
     collName: coll.name
   }
 }
@@ -39,10 +39,10 @@ for (const [key, coll] of Object.entries(schemas.v3.dbs.bakeworks.collections)) 
 ** `{ schemaName: 'v3', dbName: 'bakeryId', collName: 'recipes' }`
 */
 const bakery = {}
-for (const [key, coll] of Object.entries(schemas.v3.dbs.bakeworks.collections)) {
+for (const [key, coll] of Object.entries(schemaV3.dbs.bakeworks.collections)) {
   bakery[key] = bakeryid => {
     return {
-      schemaName: schemas.v3.name,
+      schemaName: schemaV3.name,
       dbName: `bakery-${bakeryid}`,
       collName: coll.name
     }
@@ -60,10 +60,10 @@ for (const [key, coll] of Object.entries(schemas.v3.dbs.bakeworks.collections)) 
 ** `{ schemaName: 'v2', dbName: 'tbs-byron', collName: 'ingredients' }`
 */
 const legacyV2 = {}
-for (const db of Object.values(schemas.v2.dbs)) {
+for (const db of Object.values(schemaV2.dbs)) {
   for (const coll of Object.values(db.collections)) {
     legacyV2[key] = {
-      schemaName: schemas.v2.name,
+      schemaName: schemaV2.name,
       dbName: db.name,
       collName: coll.name
     }
@@ -71,12 +71,12 @@ for (const db of Object.values(schemas.v2.dbs)) {
 }
 
 const adminDb = {
-  schemaName: v3.name,
+  schemaName: schemaV3.name,
   dbName: 'admin',
 }
 
 export default {
-  schemas,
+  schemas: { v2: schemaV2, v3: schemaV3 },
   resolve,
   bakeworks,
   bakery,
