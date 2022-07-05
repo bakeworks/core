cf5b459bd7bd1c31f394b08ebc2c0b0f13581f2e
bakeworks <mail@bake.works>
2022-07-05 15:49:22 +1000

    work in progress

--- src/db/target.js
+++ src/db/target.js
@@ -1,7 +1,7 @@
-import { v2 as schemaV2, v3 as schemaV3 } from './domains/index.js'
+import { v2 as domainV2, v3 as domainV3 } from './domains/index.js'
 
 // args: { domainName, dbName, collName }
-// if domainName is undefined set to V3 schema name
+// if domainName is undefined set to V3 domain name
 // return object { domainName, dbName, collName }
 function resolve(args) {
   const { domainName = v3.name, dbName, collName } = args
@@ -20,10 +20,10 @@ function resolve(args) {
 */
 const bakeworks = {}
 // console.debug(`target.js: v3=${v3}  v3=${v3}  v3.dbs=${v3.dbs}  v3.dbs.bakeworks=${v3.dbs.bakeworks}  v3.dbs.bakeworks.collections=${v3.dbs.bakeworks.collections}`)
-for (const [key, coll] of Object.entries(schemaV3.dbs.bakeworks.collections)) {
+for (const [key, coll] of Object.entries(domainV3.dbs.bakeworks.collections)) {
   bakeworks[key] = {
-    domainName: schemaV3.name,
-    dbName: schemaV3.dbs.bakeworks.name,
+    domainName: domainV3.name,
+    dbName: domainV3.dbs.bakeworks.name,
     collName: coll.name
   }
 }
@@ -39,10 +39,10 @@ for (const [key, coll] of Object.entries(schemaV3.dbs.bakeworks.collections)) {
 ** `{ domainName: 'v3', dbName: 'bakeryId', collName: 'recipes' }`
 */
 const bakery = {}
-for (const [key, coll] of Object.entries(schemaV3.dbs.bakeworks.collections)) {
+for (const [key, coll] of Object.entries(domainV3.dbs.bakeworks.collections)) {
   bakery[key] = bakeryid => {
     return {
-      domainName: schemaV3.name,
+      domainName: domainV3.name,
       dbName: `bakery-${bakeryid}`,
       collName: coll.name
     }
@@ -60,10 +60,10 @@ for (const [key, coll] of Object.entries(schemaV3.dbs.bakeworks.collections)) {
 ** `{ domainName: 'v2', dbName: 'tbs-byron', collName: 'ingredients' }`
 */
 const legacyV2 = {}
-for (const db of Object.values(schemaV2.dbs)) {
+for (const db of Object.values(domainV2.dbs)) {
   for (const [key, coll] of Object.entries(db.collections)) {
     legacyV2[key] = {
-      domainName: schemaV2.name,
+      domainName: domainV2.name,
       dbName: db.name,
       collName: coll.name
     }
@@ -71,12 +71,12 @@ for (const db of Object.values(schemaV2.dbs)) {
 }
 
 const adminDb = {
-  domainName: schemaV3.name,
+  domainName: domainV3.name,
   dbName: 'admin',
 }
 
 export default {
-  schemas: { v2: schemaV2, v3: schemaV3 },
+  domains: { v2: domainV2, v3: domainV3 },
   resolve,
   bakeworks,
   bakery,
